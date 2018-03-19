a00cc9e0839339c7df0d2868629202321d2847a8
Jesse Spielman <jesse.spielman@framestore.com>
2018-03-19 11:36:04 -0400

    Remove some silly debug messages

--- splish_splash.cpp
+++ splish_splash.cpp
@@ -10,18 +10,15 @@
 typedef void (*orig_setpixmap_f_type)(QSplashScreen *, const QPixmap &pixmap);
 
 void QSplashScreen::setPixmap(const QPixmap &pixmap){
-    printf("YO YO YO\n\n\n\n");
 
     const char* img_path = std::getenv("CM_HACK_NUKE_SPLASH");
-    printf(img_path);
-    printf("\n");
+    printf("hacked splash: %s\n", img_path);
 
     QPixmap hacked_pixmap;
     if(!hacked_pixmap.load(img_path)){
         hacked_pixmap = pixmap;
     }
 
-    // OPTION 1: Attempt to grab OG setPixmap function and pass along fake image to that...
     orig_setpixmap_f_type orig_setpixmap;
     orig_setpixmap = (orig_setpixmap_f_type)dlsym(RTLD_NEXT, "_ZN13QSplashScreen9setPixmapERK7QPixmap");
     orig_setpixmap(this, hacked_pixmap);
