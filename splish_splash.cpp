c8d35e2849adfb93cbcf68dd278eae9117e073f1
Jesse Spielman <jesse.spielman@framestore.com>
2018-03-19 11:59:34 -0400

    Add some comment signposting

--- splish_splash.cpp
+++ splish_splash.cpp
@@ -14,13 +14,18 @@ typedef void (*orig_setpixmap_f_type)(QSplashScreen *, const QPixmap &pixmap);
 void QSplashScreen::setPixmap(const QPixmap &pixmap){
 
     const char* img_path = std::getenv(ENV_VAR_NAME);
+
+    // At least let people know something is afoot...
     printf("hacked splash: %s\n", img_path);
 
+    // If loading the hacked pixmap doesn't work, fall back on the original
     QPixmap hacked_pixmap;
     if(!hacked_pixmap.load(img_path)){
         hacked_pixmap = pixmap;
     }
 
+    // Use some nuts function pointer black magic to replace the
+    // compiled QSplashScreen::setPixmap() with ours...
     orig_setpixmap_f_type orig_setpixmap;
     orig_setpixmap = (orig_setpixmap_f_type)dlsym(RTLD_NEXT, FUNC_SYMBOL);
     orig_setpixmap(this, hacked_pixmap);
